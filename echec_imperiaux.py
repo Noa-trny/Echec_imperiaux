afadb8e632db03e6b20b7211fbd99e1b0bd7c0a6
noah-tournant <noah.tournant@supinfo.com>
2024-11-19 11:22:26 +0100

    ajout des selection de pion

--- echec_imperiaux.py
+++ echec_imperiaux.py
@@ -1,47 +1,35 @@
 import tkinter as tk
 
-
-class joueur:
-    coordonnees_reine = []
-    nbr_pions = 0
-    type_pion1 = "reine"
-    type_pion2 = "pion"
-
-    def __init__(self, coordonnees_reine, nbr_pions):
-        self.coordonnees_reine = coordonnees_reine
-        self.nbr_pions = nbr_pions
-
-
-
 class Jeu:
     def __init__(self, n):
         self.n = n
-        self.plateau = [] 
-
+        self.tableau = self.create_plateau()
+        
     def create_plateau(self):
-        liste1 = []
-        for i in range(self.n // 2):
-            liste2 = []
+        self.root = tk.Tk()
+        liste1 = []  
+        for i in range(self.n // 2):  
+            liste2 = []  
             for j in range(self.n):
                 if j >= self.n // 2:
                     if i == 0 and j == self.n-1:
-                        btn = tk.Button(self.root, bg="orange", width=4, height=2)
+                        btn = tk.Button(self.root, bg="orange", width=4, height=2, command=lambda i=i, j=j: self.selectedpion(i,j))
                     else:
-                        btn = tk.Button(self.root, bg="red", width=4, height=2)
+                        btn = tk.Button(self.root, bg="red", width=4, height=2, command=lambda i=i, j=j: self.selectedpion(i,j))
                 else:
                     btn = tk.Button(self.root, bg="white", width=4, height=2)
                 btn.grid(row=i, column=j)
                 liste2.append(btn)
             liste1.append(liste2)
 
-        for i in range(self.n // 2, self.n):
+        for i in range(self.n // 2, self.n):  
             liste2 = []
             for j in range(self.n):
                 if j < self.n // 2:
                     if i == self.n - 1 and j == 0:
-                        btn = tk.Button(self.root, bg="purple", width=4, height=2)
+                        btn = tk.Button(self.root, bg="purple", width=4, height=2, command=lambda i=i, j=j: self.selectedpion(i,j))
                     else:
-                        btn = tk.Button(self.root, bg="blue", width=4, height=2)
+                        btn = tk.Button(self.root, bg="blue", width=4, height=2, command=lambda i=i, j=j: self.selectedpion(i,j))
                 else:
                     btn = tk.Button(self.root, bg="white", width=4, height=2)
                 btn.grid(row=i, column=j)
@@ -51,10 +39,38 @@ class Jeu:
         return liste1
 
     def UI(self):
-        self.root = tk.Tk()
         self.root.title("Game")
-        self.create_plateau()
+        self.tableau
+
+        self.label_joueur = tk.Label(self.root, text="Joueur 1")
+        self.label_joueur.grid(row=self.n, columnspan=self.n)
         self.root.mainloop()
 
-jeu = Jeu(10)
+    def selectedpion(self, i, j):
+    
+        self.tableau[i][j].config(bg='green')
+
+        
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        for di, dj in directions:
+            k = 1
+            while True:
+                ni, nj = i + k * di, j + k * dj
+            
+                if 0 <= ni < self.n and 0 <= nj < self.n:
+            
+                    if self.tableau[ni][nj].cget("bg") == 'white':
+                        self.tableau[ni][nj].config(bg='brown')
+                        k += 1
+                    else:
+                        break  
+                else:
+                    break 
+
+    def move(self,i,j):
+        pass
+        
+
+jeu = Jeu(8)
 jeu.UI()
+
